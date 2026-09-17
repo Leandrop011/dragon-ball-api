27ddefa69764b5893ad7824dc231c205bc9d89fc
Leandro Pozo <jordanspozo@gmail.com>
2026-09-17 16:38:36 -0500

    Init: in this section I will add the configurations for implements the data base and implements the endpoints get/patch/delete.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { DragonBallModule } from './dragon-ball/dragon-ball.module';
 
+// ! SECTION 01 DATA BASE AND ENDPOINTS CONFIG
+
 @Module({
   imports: [DragonBallModule],
   controllers: [],
